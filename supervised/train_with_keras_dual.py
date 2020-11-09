d446f2e855254f4ef27a80deb89514b3bd194402
Emre Fisne <emre@fisne.com>
2020-11-09 11:48:42 +0300

    dual traininig:1node+12node,train and validation split,loss and accuracy graphs

--- supervised/train_with_keras_dual.py
+++ supervised/train_with_keras_dual.py
@@ -22,8 +22,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #prepare data
 samples_1 = np.load('samples_1.npy')
-x_1 = samples_1[:,0]
-y_1 = samples_1[:,1]
+samples_1_train = samples_1[:-1000]#ilk 4000 line
+samples_1_valid = samples_1[-1000:]#sonraki 1000 line
+
+x_1 = samples_1_train[:,0]
+y_1 = samples_1_train[:,1]
+
+x_1_valid = samples_1_valid[:,0]
+y_1_valid = samples_1_valid[:,1]
+y_1_valid_onehot = to_categorical(y_1_valid, num_classes=4)
 
 y_onehot_1 = to_categorical(y_1, num_classes=4) #one-hot encoding, y train(labels)
 
@@ -40,7 +47,7 @@ mynet_1 = KerasNetwork_1(input_dim_1, output_dim_1)
 mynet_1.model.compile(loss = "categorical_crossentropy", optimizer = optimizer1, metrics=["accuracy"])
 
 #train
-history_1 = mynet_1.model.fit(x = x_1, y = y_onehot_1, batch_size = 64, epochs = 300, verbose = 1)
+history_1 = mynet_1.model.fit(x = x_1, y = y_onehot_1, batch_size = 64, epochs = 300, validation_data = (x_1_valid,y_1_valid_onehot), verbose = 1)
 
 print("training_1 completed")
 
@@ -48,8 +55,15 @@ print("training_1 completed")
 ##############################################################################################################
 #prepare data
 samples_12 = np.load('samples_12.npy')
-x_12 = samples_12[:,:-1]
-y_12 = samples_12[:,-1]
+samples_12_train = samples_12[:-1000]#ilk 4000 line
+samples_12_valid = samples_12[-1000:]#sonraki 1000 line
+
+x_12 = samples_12_train[:,:-1]
+y_12 = samples_12_train[:,-1]
+
+x_12_valid = samples_12_valid[:,:-1]
+y_12_valid = samples_12_valid[:,-1]
+y_12_valid_onehot = to_categorical(y_12_valid, num_classes=4)
 
 y_onehot_12 = to_categorical(y_12, num_classes=4) #one-hot encoding, y train(labels)
 
@@ -59,24 +73,29 @@ output_dim_12 = 4
 mynet_12 = KerasNetwork_12(input_dim_12, output_dim_12)
 
 #model compile
+optimizer1 = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
 mynet_12.model.compile(loss = "categorical_crossentropy", optimizer = optimizer1, metrics=["accuracy"])
 
 #train
-history_12 = mynet_12.model.fit(x = x_12, y = y_onehot_12, batch_size = 64, epochs = 300, verbose = 1)
+history_12 = mynet_12.model.fit(x = x_12, y = y_onehot_12, batch_size = 64, epochs = 300, validation_data = (x_12_valid,y_12_valid_onehot), verbose = 1)
 
 print("training_12 completed")
 
 #logs
-plt.plot(history_1.history['loss'], color='b', label="loss(1)")
-plt.plot(history_12.history['loss'], color='r', label="loss(12)")
+plt.plot(history_1.history['loss'], color='b', label="loss(1node)")
+plt.plot(history_1.history['val_loss'], color='c', label="val_loss(1node)")
+plt.plot(history_12.history['loss'], color='r', label="loss(12node)")
+plt.plot(history_12.history['val_loss'], color='m', label="val_loss(12node)")
 plt.title("CE loss vs. epochs")
 plt.xlabel("epochs")
 plt.ylabel("CE loss")
 plt.legend()
 plt.show()
 
-plt.plot(history_1.history['acc'], color='b', label="accuracy(1)")
-plt.plot(history_12.history['acc'], color='r', label="accuracy(12)")
+plt.plot(history_1.history['acc'], color='b', label="accuracy(1node)")
+plt.plot(history_1.history['val_acc'], color='c', label="val_accuracy(1node)")
+plt.plot(history_12.history['acc'], color='r', label="accuracy(12node)")
+plt.plot(history_12.history['val_acc'], color='m', label="val_accuracy(12node)")
 plt.title("accuracy vs. epochs")
 plt.xlabel("epochs")
 plt.ylabel("accuracy")
